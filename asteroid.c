e9b3b4ee7bffc8b82f0930fbc5b799359f8307d3
Manuel Rodríguez Sutil <mrodsut@gmail.com>
2026-02-05 19:03:53 +0000

    started resolveCollisions method

--- asteroid.c
+++ asteroid.c
@@ -6,6 +6,7 @@
 #include "raylib.h"
 #include "raymath.h"
 
+#include <math.h>
 #include <stdlib.h>
 
 #define WIDTH 800
@@ -14,10 +15,6 @@
 #define NUM_ASTEROIDS 6
 #define SCALE 0.8f
 #define VEL 2.0f
-
-typedef struct {
-    int vx, vy;
-} Velocity;
 
 typedef struct {
     Vector2 pos;
@@ -51,6 +48,38 @@ void initAsteroids() {
         float mass = radius * radius;
         ASTEROIDS[i] = (Asteroid){pos, sides, radius, rotation, getRandV(), mass};
     }
+}
+
+// TODO: Finish this
+void resolveCollisions(Asteroid* a, Asteroid* b) {
+    // Find unit normal and unit tangent vectors
+    Vector2 delta = Vector2Subtract(a->pos, b->pos);
+    float dsq = Vector2LengthSqr(delta);
+    float rsum = a->radius + b->radius;
+
+    if (dsq <= 0.0000001f || dsq >= rsum*rsum) return;
+
+    float dist = sqrtf(dsq);
+    Vector2 unitNormal = Vector2Scale(delta, 1.0f/dist);
+    Vector2 unitTangent = (Vector2){-unitNormal.y, unitNormal.x};
+    
+    // Create initial (before the collision velovty vector, v1 and v2) -- Already done
+    // Resolve the velocity vectors (v1 and v2) into normal and angential components. Project the velocity vectors ontro the unit normal and the unit tangent vectors by taking the dot product of the vlocity vectors with the normal and unit tangent vectors. Vn and Vt are scalars.
+    float velANormal = Vector2DotProduct(a->vel, unitNormal);
+    float velATangent = Vector2DotProduct(a->vel, unitTangent);
+    float velBNormal = Vector2DotProduct(b->vel, unitNormal);
+    float velBTangent = Vector2DotProduct(b->vel, unitTangent);
+
+    // Find the new tangential velocities (after the collision). They do not change: v1_t' = v1_t, v2_t' = v2_t.
+    // Find the new normal velocities (after the collision ("prime")). Same as one-dimensional collision formulas:
+    // v1_n' = v1_n * (m1 - m2) + 2*m2*v2_n / m1 + m2
+    // v2_n' = v2_n * (m1 - m2) + 2*m1*v1_n / m1 + m2
+
+    // Convert the scalar normal and tangential velocities into vectors. Multiply the unit normal vector by the scalar normal velocity, same for the tangential component
+
+    // Find the final volicyt vectors by adding the normal and tangential components for each object:
+    // v1' = v1_n' + v1_t'
+    // v2' = v2_n' + v2_t'
 }
 
 void checkCollisions() {
@@ -140,9 +169,9 @@ int main(void)
     {
         BeginDrawing();
             ClearBackground(BLACK);
+			UpdateGame(&spaceship);
             DrawAsteroids();
             DrawSpaceShip(&spaceship);
-			UpdateGame(&spaceship);
         EndDrawing();
     }
 
